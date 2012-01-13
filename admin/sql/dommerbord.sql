ee310925dd4580ef6c88666edc8e23ed2a09d167
Martin Juhl <mj@casalogic.dk>
2012-01-14 00:35:52 +0100

    Version++ to 1.1.8..
    Ny sql struktur dump..

--- admin/sql/dommerbord.sql
+++ admin/sql/dommerbord.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 28, 2011 at 12:34 AM
+-- Generation Time: Jan 14, 2012 at 12:35 AM
 -- Server version: 5.1.58
 -- PHP Version: 5.3.6-13ubuntu3.3
 
@@ -23,6 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `calendars`
+--
+
+CREATE TABLE IF NOT EXISTS `calendars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) NOT NULL,
+  `team` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=373 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `config`
 --
 
@@ -37,6 +50,76 @@ CREATE TABLE IF NOT EXISTS `config` (
   `updatesurl` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `position` int(8) unsigned NOT NULL DEFAULT '0',
+  `text` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `dt_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `refereeteam1id` int(8) NOT NULL,
+  `referee1id` int(8) NOT NULL,
+  `refereeteam2id` int(8) NOT NULL,
+  `referee2id` int(8) NOT NULL,
+  `tableteam1id` int(8) NOT NULL,
+  `table1id` int(8) NOT NULL,
+  `tableteam2id` int(8) NOT NULL,
+  `table2id` int(8) NOT NULL,
+  `tableteam3id` int(8) NOT NULL,
+  `table3id` int(8) NOT NULL,
+  `status` int(8) NOT NULL,
+  `place` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `position` (`position`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1000016 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `team` varchar(255) NOT NULL,
+  `playername` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `teamid` int(11) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10018 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `password` text NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `email` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
-  $dommerplanversion="1.1.7";
+  $dommerplanversion="1.1.8";
 
 ?>
