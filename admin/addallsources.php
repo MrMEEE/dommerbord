50b0fe92ae6efb2c39dc0731b65b9cd3969bff5c
Martin Juhl <mj@casalogic.dk>
2012-01-26 00:19:35 +0100

    Addallsources.php cleanup..

--- admin/addallsources.php
+++ admin/addallsources.php
@@ -1,159 +1,121 @@
 <?php
 
-require "connect.php";
-require "checkLogin.php";
-require "checkAdmin.php";
-require "config.php";
+require("connect.php");
+require("checkLogin.php");
+require("checkAdmin.php");
+require("config.php");
+require("theme.php");
+
+getThemeHeader();
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script type="text/javascript">
-
-function ConfirmChoice(teamid)
-
-{
+function ConfirmChoice(teamid){
  
  answer = confirm("Er du sikker på at du vil slette dette Hold/denne Pulje");
  
- if (answer !=0)
- {
+ if (answer !=0){
   
   var klubadresse = "<?php echo $klubadresse;?>";
   var klubpath = "<?php echo $klubpath;?>";
   
   location = "http://" + klubadresse + "/" + klubpath + "/admin/addallsources.php?removesource=1&id=" + teamid;
   
-  }
+ }
    
-  }
+}
 
-function ConfirmAllChoice()
-
-{
+function ConfirmAllChoice(){
  
   answer = confirm("Er du sikker på at du vil slette alle Hold/Puljer");
    
-  if (answer !=0)
-     {
+  if (answer !=0){
        
    var klubadresse = "<?php echo $klubadresse;?>";
    var klubpath = "<?php echo $klubpath;?>";
            
-  location = "http://" + klubadresse + "/" + klubpath + "/admin/addallsources.php?removeallsources=1";
+   location = "http://" + klubadresse + "/" + klubpath + "/admin/addallsources.php?removeallsources=1";
                
-   }
+  }
                   
 }
-                      
-    
-</script>
-    
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?php echo $klubnavn; ?> Dommerbordsplan</title>
 
-<!-- Including the jQuery UI Human Theme -->
-<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/themes/humanity/jquery-ui.css" type="text/css" media="all" />
+<?php                      
 
-<!-- Our own stylesheet -->
-<link rel="stylesheet" type="text/css" href="styles.css" />
+getThemeTitle("Holdadministration");    
 
-</head>
+require("menu.php");
 
-<body>
-
-<h1><?php echo $klubnavn; ?> Dommerplan</h1>
-
-<div id="main">
-
-<?php require("menu.php"); ?>
-
-<?php
-if (checkAdmin($_SESSION['username'])){
+if(checkAdmin($_SESSION['username'])){
 
 if(isset($_GET["removesource"])){
-    if(isset($_GET["id"])){
-      $id=$_GET["id"];
-      mysql_query("DELETE FROM `calendars` WHERE `id` = $id");
-      echo "Hold/Pulje slettet.<br>";
-      }
+ if(isset($_GET["id"])){
+  $id=$_GET["id"];
+  mysql_query("DELETE FROM `calendars` WHERE `id` = $id");
+  echo "Hold/Pulje slettet.<br>";
+ }
 }elseif(isset($_GET["removeallsources"])){
-    mysql_query("DELETE FROM `calendars`");
-    echo "Alle Hold/Puljer slettet.<br>";
-    
+ mysql_query("DELETE FROM `calendars`");
+ echo "Alle Hold/Puljer slettet.<br>";    
 }elseif(isset($_GET["refreshsources"])){
+ $url        = "http://resultater.basket.dk/tms/Turneringer-og-resultater/Forening-Holdoversigt.aspx?ForeningsId=$klubid";
+ $input = @file_get_contents($url) or die("Could not access file: $url");
+ $regexp = "PuljeId=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+ $regexp2 = "RaekkeId=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
 
-  //$dom = new DOMDocument();
-  $url        = "http://resultater.basket.dk/tms/Turneringer-og-resultater/Forening-Holdoversigt.aspx?ForeningsId=$klubid";
-  $input = @file_get_contents($url) or die("Could not access file: $url");
-  $regexp = "PuljeId=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
-  $regexp2 = "RaekkeId=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
+ preg_match_all("/$regexp/siU", $input, $matches);
+ preg_match_all("/$regexp2/siU", $input, $matches2);
   
-  if(preg_match_all("/$regexp/siU", $input, $matches)) { 
-  // $matches[2] = array of link addresses // $matches[3] = array of link text - including HTML code 
-  }
-  if(preg_match_all("/$regexp2/siU", $input, $matches2)) {
-  
-  }
-  $i=0;
-  $newteams=0;
-  foreach ($matches[2] as $urls){
-   $name=$matches2[3][$i];
-    if(mysql_num_rows(mysql_query("SELECT * FROM calendars WHERE address = 'http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId=$urls'"))){
-    if($debug != 0){ 
-      echo $urls;
-      echo " - ";
-      echo "$name already exists<br>";
-    }
-    }
-    else{
-        mysql_query("INSERT into calendars (`address`, `team`) VALUES ('http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId=$urls', '$name')");
-    
- print('Tilføjet: <a href="http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId='.$urls.'">'.$name.'</a><br>');
- 
- 
-  $newteams=1;
-  
+ $i=0;
+ $newteams=0;
+ foreach ($matches[2] as $urls){
+  $name=$matches2[3][$i];
+  if(mysql_num_rows(mysql_query("SELECT * FROM calendars WHERE address = 'http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId=$urls'"))){
+   if($debug != 0){ 
+    echo $urls;
+    echo " - ";
+    echo "$name already exists<br>";
+   }
+  }else{
+   mysql_query("INSERT into calendars (`address`, `team`) VALUES ('http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId=$urls', '$name')");
+   print('Tilføjet: <a href="http://resultater.basket.dk/tms/Turneringer-og-resultater/Pulje-Komplet-Kampprogram.aspx?PuljeId='.$urls.'">'.$name.'</a><br>');
+   $newteams=1;
   }
   $i=$i+1;
-  }
+ }
   
-  if($newteams==0){
-   echo "Ingen nye Hold/Puljer.<br>";
-  }
+ if($newteams==0){
+  echo "Ingen nye Hold/Puljer.<br>";
+ }
 }
 
 }
 
 $query = mysql_query("SELECT * FROM `calendars` ORDER BY `team`");
 
-// Filling the $todos array with new ToDo objects:
 if (checkAdmin($_SESSION['username'])){
-
-echo '<br><a href="addallsources.php?refreshsources=1">Tilføj alle klubbens hold/puljer</a><br>';
-echo '<br><a href="javascript:void(ConfirmAllChoice())">Fjern alle klubbens hold/puljer</a><br>';
+ echo '<br><a href="addallsources.php?refreshsources=1">Tilføj alle klubbens hold/puljer</a><br>';
+ echo '<br><a href="javascript:void(ConfirmAllChoice())">Fjern alle klubbens hold/puljer</a><br>';
 }
+
 echo '<br><br>Hold: <br><br>';
 
 while($row = mysql_fetch_assoc($query)){
-    echo '<a href="';
-    echo $row['address'];
-    echo '">';
-    echo $row['team'];
-    echo '</a>';
-    if (checkAdmin($_SESSION['username'])){
-    echo ' - ';
-    echo '<a href="javascript:void(ConfirmChoice('.$row['id'].'))">Fjern</a>';    
-    }
-    echo '<br>';
+ echo '<a href="';
+ echo $row['address'];
+ echo '">';
+ echo $row['team'];
+ echo '</a>';
+ if (checkAdmin($_SESSION['username'])){
+  echo ' - ';
+  echo '<a href="javascript:void(ConfirmChoice('.$row['id'].'))">Fjern</a>';    
+ }
+ echo '<br>';
 }
                   
 echo '<br><br>';
+
+getThemeBottom();
+
 ?>
 
-</div>
-
-</body>
-</html>
